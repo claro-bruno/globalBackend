66db73d78b9c68f1b30db63dd3161349a4203f5a
Bruno Augusto <brunaugusto@gmail.com>
2022-11-24 22:37:46 -0300

    atualiza

--- src/api/app.ts
+++ src/api/app.ts
@@ -13,7 +13,7 @@ app.use("/images", express.static(path.resolve(__dirname, "../../uploads")));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(runGenerateQuarterJobs);
+// app.use(runGenerateQuarterJobs);
 app.use(router);
 app.use(errorHandler);
 
