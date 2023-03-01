714c3c3062fb99f470780fed73be01a574841ff1
Bruno Augusto <brunaugusto@gmail.com>
2023-03-01 14:15:21 -0300

    updating order

---
+++ src/modules/orders/useCases/UpdateInvoiceOrder/UpdateInvoiceOrderUseCase.ts
@@ -0,0 +1,49 @@
+import { prisma } from "../../../../database/prismaClient";
+import { AppError } from "../../../../middlewares/AppError";
+
+interface IUpdateOrder {
+    id: number;
+    invoice: string;
+}
+
+
+export class UpdateInvoiceOrderUseCase {
+    async execute({ id, invoice } : IUpdateOrder): Promise<any>{
+        
+       
+        //validar se o client existe
+        const orderExist = await prisma.orders.findFirst({
+           where: {
+               id,
+           }
+        });
+
+        if(!orderExist || invoice==='') {
+            throw new AppError('Order does not exists', 401)
+        }
+        
+        
+
+       
+        
+        await prisma.orders.update({
+            where: {
+                id,
+            },
+            data: {
+                fk_invoice_id: invoice
+            }
+        });
+        
+        
+           
+    
+           
+            
+        return 'ok';
+
+    }
+        
+        
+        
+}
--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -6,6 +6,7 @@ import { CreateOrderController } from "../modules/orders/useCases/CreateOrder/Cr
 import { UpdateOrderController } from "../modules/orders/useCases/UpdateOrder/UpdateOrderController";
 import { GetOrderController } from "../modules/orders/useCases/GetOrder/GetOrderController";
 import { GetOrdersController } from "../modules/orders/useCases/GetOrders/GetOrdersController";
+import { UpdateInvoiceOrderController } from "../modules/orders/useCases/UpdateInvoiceOrder/UpdateInvoiceOrderController";
 
 
 
@@ -16,8 +17,10 @@ const createOrderController = new CreateOrderController();
 const updateOrderController = new UpdateOrderController();
 const getOrderController =  new GetOrderController();
 const getOrdersController = new GetOrdersController();
+const udateInvoiceOrderController = new UpdateInvoiceOrderController();
 
 ordersRoutes.post("/", use(ensureAuthenticate), use(createOrderController.handle));
+ordersRoutes.put("/invoice", use(ensureAuthenticate), use(udateInvoiceOrderController.handle));
 ordersRoutes.put("/:id", use(ensureAuthenticate), use(updateOrderController.handle));
 ordersRoutes.get("/:id", use(ensureAuthenticate), use(getOrderController.handle));
 ordersRoutes.get("/", use(ensureAuthenticate), use(getOrdersController.handle));
