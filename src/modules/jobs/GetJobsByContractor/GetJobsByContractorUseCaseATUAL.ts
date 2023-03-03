461f530a6e165146e174d91016398b34e39105fc
Bruno Augusto <brunaugusto@gmail.com>
2023-03-03 13:40:58 -0300

    add alter contractor page

--- src/modules/jobs/GetJobsByContractor/GetJobsByContractorUseCaseATUAL.ts
+++ src/modules/jobs/GetJobsByContractor/GetJobsByContractorUseCaseATUAL.ts
@@ -11,109 +11,36 @@ function toJson(data: any) {
 
 export class GetJobsByContractorUseCase {
   async execute(id: number, year: number, month: string) {
-    const result: any = [];
-    let arr: any = [];
-
-    const jobs_quarters = await prisma.quarters.findMany({
-      orderBy: [
-        {
-          // fk_id_job: 'asc',
-          id: "asc"
-        }
-      ],
-      where: {
-        month,
-        year: +year,
-        jobs: {
-          fk_id_contractor: +id,
-        }
-        
-      },
-      select: {
-        jobs: {
-          select: {
-            status: true,
-            id: true,
-            sunday: true,
-            monday: true,
-            tuesday: true,
-            wednesday: true,
-            thursday: true,
-            friday: true,
-            saturday: true,
-            start: true,
-            end: true,
-            client: {
-              select: {
-                name: true,
-                id: true
-              }
-            },
-            contractor: {
-              select: {
-                first_name: true,
-                middle_name: true,
-                last_name: true,
-                id: true
-              }
-            }
-          }
-        },
-        fk_id_job: true,
-        id: true,
-        order: true,
-        month: true,
-        year: true,
-        value_hour: true,
-        status: true,
-        others: true,
-        appointment: true,
-      }
-    });
-    
-    const jobsGrouped = groupBy(
-      jobs_quarters,
-      (quarter: any) => quarter.fk_id_job
-    );
-
-    let totals_days: any = await prisma.$queryRaw`
-      SELECT SUM(A.value) AS totalHour, extract(day from A.date) as dia, A.date from appointments A
-      INNER JOIN quarters Q ON Q.id = A.fk_id_quarter
-      INNER JOIN jobs J ON J.id = Q.fk_id_job
-      WHERE q.month = ${month} AND q.year= ${year} AND J.fk_id_contractor= ${id}
-      GROUP BY extract(day from A.date), A.date 
-      ORDER BY extract(day from A.date)
-    `
-
-
-    // let result: any = await prisma.$queryRaw`
-    //         SELECT
-    //         j.id,
-    //         -- ap.id as id_ap,
-    //         ap.date,
-    //         ap.value,
-    //         q.value_hour,            
-    //         c.name,
-    //         c.id,
-    //         ap.fk_id_quarter,
-    //         q.month,
-    //         q.others,
-    //         j.sunday,
-    //         j.monday,
-    //         j.tuesday,
-    //         j.wednesday,
-    //         j.thursday,
-    //         j.friday,
-    //         j.saturday,
-    //         j.start,
-    //         j.end
-    //         FROM appointments ap
-    //         INNER JOIN quarters q ON q.id = ap.fk_id_quarter
-    //         INNER JOIN jobs j ON j.id = q.fk_id_job
-    //         INNER JOIN clients c ON c.id = j.fk_id_client
-    //         WHERE  q.year = ${year} AND q.month = ${month} AND j.fk_id_contractor = ${id} AND j.status = 'ACTIVE'
-    //         ORDER BY j.id, ap.fk_id_quarter, ap.date ASC
-    //         ;`;
+    // let result: any = [];
+    // let arr: any = [];
+    let result: any = await prisma.$queryRaw`
+            SELECT
+            j.id,
+            -- ap.id as id_ap,
+            ap.date,
+            ap.value,
+            q.value_hour,            
+            c.name,
+            c.id,
+            ap.fk_id_quarter,
+            q.month,
+            q.others,
+            j.sunday,
+            j.monday,
+            j.tuesday,
+            j.wednesday,
+            j.thursday,
+            j.friday,
+            j.saturday,
+            j.start,
+            j.end
+            FROM appointments ap
+            INNER JOIN quarters q ON q.id = ap.fk_id_quarter
+            INNER JOIN jobs j ON j.id = q.fk_id_job
+            INNER JOIN clients c ON c.id = j.fk_id_client
+            WHERE  q.year = ${year} AND q.month = ${month} AND j.fk_id_contractor = ${id} AND j.status = 'ACTIVE'
+            ORDER BY j.id, ap.fk_id_quarter, ap.date ASC
+            ;`;
     // result = toJson(result);
     // result = JSON.parse(result);
     // const result = await prisma.appointments.findMany({
@@ -235,76 +162,16 @@ export class GetJobsByContractorUseCase {
           GROUP BY q.order
     ;`;
     
-    const result_totals: any = await prisma.$queryRaw`
-            SELECT 
-            q.fk_id_job as id,
-            j.status,
-            q.id AS quarter_id,
-            q.order,
-            CONCAT(c.first_name,' ',c.middle_name,' ',c.last_name) AS contractor_name,
-            CONCAT(cl.name) AS client_name,
-            sum(ap.value*q.value_hour) total,
-            sum(ap.value) total_hours
-            FROM jobs j
-            INNER JOIN quarters q ON q.fk_id_job = j.id
-            INNER JOIN appointments ap ON ap.fk_id_quarter = q.id
-            INNER JOIN contractors c ON c.id = j.fk_id_contractor
-            INNER JOIN clients cl ON cl.id = j.fk_id_client
-            WHERE  q.year = ${year} AND q.month = ${month} AND j.fk_id_contractor= ${id}
-            GROUP BY q.id,contractor_name,client_name,j.status
-            ORDER BY q.id ASC
-            ;`;
     
-    if (jobs_quarters.length > 0) {
       
-      jobsGrouped.forEach((job: any) => {
-        const job_info: any = {};
 
-        const {
-          id,
-          status
-          // client: { name, id: client_id },
-          // contractor: { first_name, last_name , id: contractor_id }
-        } = job[0].jobs;
-        job_info.id = id; 
-        job_info.contractor = job[0].jobs.contractor;
-        job_info.client = job[0].jobs.client;
-        job_info.name = `${job[0].jobs.contractor.first_name} ${job[0].jobs.contractor.last_name}` ;
-        // job_info.contractor_id = contractor_id;
-        // job_info.client_name = name;
-        // job_info.client_id = client_id;
-        job_info.status = status;
-        
-        job.forEach((quarter: any) => {
-          quarter.appointment.forEach((ap: any) =>{
-            ap.id = ap.id.toString().split('n')[0] !== '' ? Number(ap.id.toString().split('n')[0]) : ap.id
-          })
-          quarter.appointment.sort(function(a:any, b:any) { 
-            return a.date.getTime() - b.date.getTime() 
-          });
-          const totals = result_totals.find(
-            (info: any) => info.quarter_id === quarter.id
-          );
-          quarter.total = totals.total;
-          quarter.total_hours = totals.total_hours;
-        });
+    if (result.length > 0 && results_total_quarter.length > 0) {
 
-        job_info.quarter = job;
-       
-        result.push(job_info);
-      });
-
-      result.sort(function (a: any, b: any) {
-        if (a.name > b.name) {
-          return 1;
-        }
-        if (a.name < b.name) {
-          return -1;
-        }
-        // a must be equal to b
-        return 0;
-      });
-
+      const total_1 = results_total_quarter[0] ? results_total_quarter[0].total : 0
+      const total_2 = results_total_quarter[1] ? results_total_quarter[1].total : 0
+      const total_hours_1 = results_total_quarter[0] ? results_total_quarter[0].total_hours : 0
+      const total_hours_2 = results_total_quarter[1] ? results_total_quarter[1].total_hours : 0
+         
       const {
         total,
         total_hours,
@@ -312,37 +179,6 @@ export class GetJobsByContractorUseCase {
         total_others_1,
         total_others_2
       } = results_total[0];
-
-      const total_1 = results_total_quarter[0] ? results_total_quarter[0].total : 0
-      const total_2 = results_total_quarter[1] ? results_total_quarter[1].total : 0
-      const total_hours_1 = results_total_quarter[0] ? results_total_quarter[0].total_hours : 0
-      const total_hours_2 = results_total_quarter[1] ? results_total_quarter[1].total_hours : 0
-
-      
-      return { jobs: result, days: totals_days, totals: {
-        total: total, 
-        total_hours: total_hours, 
-        total_payment: total - total_others,
-        total_1_hours: total_hours_1,
-        total_1_quarter: total_1,
-        total_1_others: total_others_1,
-        total_1_payment: total_1 - total_others_1,
-        total_2_hours: total_hours_2,
-        total_2_quarter: total_2,
-        total_2_others: total_others_2,
-        total_2_payment: total_2 - total_others_2
-      } };
-    } else {
-      return [];
-    }
-  }
-}
-    
-      
-
-
-               
-      
       //     jobsGrouped.forEach((job: any) => {
       //         const job_info: any = {};
 
@@ -371,9 +207,34 @@ export class GetJobsByContractorUseCase {
       //         result.push(job_info);
       //     });
 
-      
-    
-  
+      return {
+        contractor_jobs: result,
+        totals: {
+          total: total, 
+          total_hours: total_hours, 
+          total_payment: total - total_others,
+          total_1_hours: total_hours_1,
+          total_1_quarter: total_1,
+          total_1_others: total_others_1,
+          total_1_payment: total_1 - total_others_1,
+          total_2_hours: total_hours_2,
+          total_2_quarter: total_2,
+          total_2_others: total_others_2,
+          total_2_payment: total_2 - total_others_2
+        }
+      };
+    } else {
+      return {
+        contractor_jobs: [],
+        totals: [
+          { total: 0, total_hours: 0, total_with_taxes: 0 },
+          { total_1hours: 0, total_1quarter: 0, total_taxes1quarter: 0, total_1: 0 },
+          { total_2hours: 0, total_2quarter: 0, total_taxes2quarter: 0, total_2: 0 }
+        ]
+      };
+    }
+  }
+}
 
 
 
