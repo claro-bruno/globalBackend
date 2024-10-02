ae208b5149709cab90a152008d01830b30abc6e7
Bruno Augusto <brunaugusto@gmail.com>
2024-10-02 14:19:32 -0300

    fix payments

--- src/modules/clients/useCases/GetClientsProfit/GetClientsProfitUseCase.ts
+++ src/modules/clients/useCases/GetClientsProfit/GetClientsProfitUseCase.ts
@@ -1,6 +1,6 @@
 
-import { prisma} from "../../../../database/prismaClient";
-import { AppError} from "../../../../middlewares/AppError";
+import { prisma } from "../../../../database/prismaClient";
+import { AppError } from "../../../../middlewares/AppError";
 
 interface IGetDataClient {
     month: string;
@@ -8,7 +8,7 @@ interface IGetDataClient {
 }
 
 export class GetClientsProfitUseCase {
-    async execute({ year, month}: IGetDataClient) {
+    async execute({ year, month }: IGetDataClient) {
 
         /**
          * "INPUT",
@@ -59,7 +59,7 @@ export class GetClientsProfitUseCase {
         //         {
         //             name: 'Support'
         //         }
-                
+
         //     }
         // });
 
@@ -86,11 +86,11 @@ export class GetClientsProfitUseCase {
             }
         });
 
-        
+
         const total_invoices_by_clients = await prisma.invoices.groupBy({
             orderBy: [{ fk_id_client: 'asc' }],
-            by: ['fk_id_client','payed_for'],
-            
+            by: ['fk_id_client', 'payed_for'],
+
             _sum: {
                 value: true
             },
@@ -106,13 +106,13 @@ export class GetClientsProfitUseCase {
             INNER JOIN appointments AS a ON a.fk_id_quarter = q.id
             WHERE j.fk_id_client = 55 AND q.month = ${month} AND q.year = ${year} AND j.fk_id_client = 80
             ;`;
-        
+
         const total_exp: any = await prisma.$queryRaw`
            SELECT SUM(p.value) AS total FROM payments AS p
             WHERE p.month = ${month} AND p.year = ${year} AND p.type != 'CONTRACTOR_WORKERS' AND p.type != 'INPUT' AND p.type != 'GLOBAL' AND p.type != 'MISC'
             ;`;
 
-        
+
 
         const total_expenses_by_type: any = await prisma.payments.groupBy({
             by: ['type'],
@@ -129,8 +129,8 @@ export class GetClientsProfitUseCase {
         arrTypes.forEach((type: string) => {
             const payments_type = total_expenses_by_type.find(
                 (info: any) => info.type === type
-              );
-              resultt[`${type}`] = typeof payments_type === 'undefined' || payments_type === null ? 0 : payments_type._sum.value as any;
+            );
+            resultt[`${type}`] = typeof payments_type === 'undefined' || payments_type === null ? 0 : payments_type._sum.value as any;
 
         });
 
@@ -145,7 +145,7 @@ export class GetClientsProfitUseCase {
                 ORDER BY j.fk_id_client ASC
          ;`
 
-        const  total_labour_total: any = await prisma.$queryRaw`
+        const total_labour_total: any = await prisma.$queryRaw`
             SELECT SUM(a.value*q.value_hour) AS total FROM jobs AS j
                 INNER JOIN quarters AS q ON q.fk_id_job = j.id
                 INNER JOIN appointments AS a ON a.fk_id_quarter = q.id
@@ -155,7 +155,7 @@ export class GetClientsProfitUseCase {
 
         const total_expenses = total_exp[0]?.total === null ? 0 : total_exp[0]?.total;
         const total_support = total_supp[0]?.total === null ? 0 : total_supp[0]?.total;
-        
+
         // const total_despesas = Number(total_expenses) + Number(total_support);
         const total_despesas = Number(total_expenses);
         const total_invoices = total_invs._sum.value == null ? 0 : total_invs._sum.value;
@@ -166,39 +166,38 @@ export class GetClientsProfitUseCase {
         let total_amount = 0;
         let total_ganho = 0;
         let total_expensive = 0;
-        console.log(total_labour_total)
         let total_labour = total_labour_total ? total_labour_total[0]?.total : 0;
         let total_profit = 0;
 
         let aux = 0;
-        if(clients.length > 0) {
+        if (clients.length > 0) {
             clients.forEach((client: any) => {
                 const info: any = {};
                 const invoice_info = total_invoices_by_clients.find(
                     (information: any) => information.fk_id_client === client.fk_id_client
-                  );
-               
+                );
+
 
                 const labour_info = total_labour_by_client.find(
                     (information: any) => information.fk_id_client === client.fk_id_client
-                  );
-                
+                );
+
 
                 // console.log(invoice_info, labour_info)
-                if(invoice_info) {
+                if (invoice_info) {
                     info.amount = invoice_info._sum.value != null ? invoice_info._sum.value : 0;
                     // info.ganho = invoice_info._sum.value != null && total_invoices > 0 ? invoice_info._sum.value / total_invoices : 0;
                     // info.expensive_value = info.ganho != null && total_despesas > 0 ? info.ganho * total_despesas : 0;
                     // info.expensive_value = total_despesas > 0 ? total_despesas / (total_invoices * info.amount) : 0;
                     info.expensive_value = total_despesas > 0 ? total_despesas / total_invoices * info.amount : 0;
-                    info.ganho = invoice_info._sum.value != null && total_invoices > 0 ?  info.expensive_value / info.amount : 0;
+                    info.ganho = invoice_info._sum.value != null && total_invoices > 0 ? info.expensive_value / info.amount : 0;
                     // info.ganho = invoice_info._sum.value != null && total_invoices > 0 ? total_despesas / invoice_info._sum.value : 0;
                     info.total_labour = labour_info ? labour_info?.total : 0;
                     info.profit = Number(Number(info.amount) - Number(info.expensive_value) - Number(info.total_labour));
                     total_amount += +info.amount
                     total_ganho += +info.ganho
                     total_expensive += +info.expensive_value
-                    
+
                     total_profit += +info.profit
                     // console.log(info)
                 }
