17a32ec43821ea7e2538a82f15e668c686dbf051
Bruno Augusto <brunaugusto@gmail.com>
2025-09-10 05:58:21 -0300

    setting changes

--- src/modules/payments/useCases/UpdateInvoices/UpdateInvoicesController.ts
+++ src/modules/payments/useCases/UpdateInvoices/UpdateInvoicesController.ts
@@ -6,8 +6,9 @@ export class UpdateInvoicesController {
         const { date_invoice, value, identification, description, id_client, taxa, total_pago, data_pagamento, method, ref, id_order, contractor_id } = request.body;
         const { id } = request.params;
         const updateInvoicesUseCase = new UpdateInvoicesUseCase();
+        const taxaNumber = Number(taxa);
         // console.log(request.body)
-        const result = await updateInvoicesUseCase.execute({ date_invoice, value, identification, description, fk_id_client: id_client, id: +id, taxa, total_pago, date_payment: data_pagamento, method, ref, fk_id_order: id_order, fk_id_contractor: contractor_id });
+        const result = await updateInvoicesUseCase.execute({ date_invoice, value, identification, description, fk_id_client: id_client, id: +id, taxa: taxaNumber, total_pago, date_payment: data_pagamento, method, ref, fk_id_order: id_order, fk_id_contractor: contractor_id });
         return response.json(result);
 
 
