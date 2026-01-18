7ff634605ea0e71e74942c2c58018ad7578a73d9
Bruno Augusto <brunaugusto@gmail.com>
2026-01-17 23:59:12 -0300

    fix

--- src/modules/materials/useCases/GetMaterials/GetMaterialsUseCase.ts
+++ src/modules/materials/useCases/GetMaterials/GetMaterialsUseCase.ts
@@ -7,7 +7,8 @@ export class GetMaterialsUseCase {
 
         const result: any = await prisma.$queryRaw`
           SELECT mat.id, mat.created_at, mat.name, mat.description, mat.unit_cost,  mat.url_image, mat.category, mat.fk_user,
-          (SELECT SUM(itr.quantity) FROM public."materialsTransactions" AS itr WHERE itr.fk_id_material = mat.id AND itr.fk_id_output = 364) AS total,
+          (SELECT SUM(itr.quantity) FROM public."materialsTransactions" AS itr WHERE itr.fk_id_material = mat.id AND itr.fk_id_output = 364) AS total_in,
+          (SELECT SUM(itr.quantity) FROM public."materialsTransactions" AS itr WHERE itr.fk_id_material = mat.id AND itr.fk_id_output != 364) AS total_out,
           (SELECT c.first_name FROM public."contractors" AS c INNER JOIN public."materialsTransactions" as it ON it.fk_id_output = c.id  WHERE c.id = mat.fk_user) AS first_name
 from public."materials" AS mat
           Order BY mat.id ASC
